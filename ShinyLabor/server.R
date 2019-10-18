0e294c17f116717685f207b2ebbba55db51b659a
kamecon <kamalspain49@gmail.com>
2019-10-18 17:27:41 +0200

    add empleo tab, reduce selectinput tabs, still not working

--- ShinyLabor/server.R
+++ ShinyLabor/server.R
@@ -23,21 +23,28 @@
 #                   estacion=NULL,
 #                   worktime=NULL){
 
-clean_eurostat_cache()
 
-dat <- get_eurostat("une_rt_a", time_format = "raw")
 
-euro2 <- function(dat,
+#clean_eurostat_cache()
+
+#dat <- get_eurostat("une_rt_a", time_format = "raw")
+# lfsi_emp_a
+# une_ltu_a
+# lfsi_educ_a
+# lfsi_pt_a
+
+euro2 <- function(concepto,
                   country,
                   sexo,
                   unidad,
-                  edad){
+                  edad,
+                  indicador){
   
   #Limpiar cach?
-  #clean_eurostat_cache()
+  clean_eurostat_cache()
   
   #Dato a buscar
-  #  dat <- get_eurostat(concepto, time_format = "raw")
+  dat <- get_eurostat(concepto, time_format = "raw")
   
   # Extract the information on country in question. (First two character in region string mark the country)
   dat$cntry <- substr(dat$geo, 1,2)
@@ -51,10 +58,20 @@ euro2 <- function(dat,
   
   #Detalle de los Datos (esto para pestania de paro)
   
-  dat %>% dplyr::filter(geo %in% c("ES", "EU27", country),
-                        unit == unidad & sex == sexo & age == edad) %>%
-    dplyr::arrange(geo, time) -> dat2
+  if (concepto=="une_rt_a") {
+    dat %>% dplyr::filter(geo %in% c("ES", "EU27", country),
+                          unit == unidad & sex == sexo & age == edad) %>%
+      dplyr::arrange(geo, time) -> dat2
+  } else if (concepto == "lfsi_emp_a") {
+    dat %>% dplyr::filter(geo %in% c("ES", "EU27", country),
+                          unit == unidad & sex == sexo & age == edad & indic_em == indicador) %>%
+      dplyr::arrange(geo, time) -> dat2
+  }
   
+  # dat %>% dplyr::filter(geo %in% c("ES", "EU27", country),
+  #                       unit == unidad & sex == sexo & age == edad & indic_em == indicador) %>%
+  #   dplyr::arrange(geo, time) -> dat2
+  # 
   # dat <- dat[dat$geo %in% c("ES", "EU27", country),]
   # dat <- dat[grepl(unidad, dat$un it),]
   # dat <- dat[grepl(sexo, dat$sex),]
@@ -102,11 +119,12 @@ shinyServer(
       #       estacion=input$estacion,
       #       worktime=input$worktime,
       #       educacion =input$educacion)
-      euro2(dat,
+      euro2(input$concepto,
         input$pais,
         input$sexo, 
         input$unidad,
-        input$edad)
+        input$edad,
+        input$indicador)
     })
     
     output$grafico <- renderPlot({
@@ -128,6 +146,28 @@ shinyServer(
       # plot
     })
     
+    output$grafico2 <- renderPlot({
+      
+      datos <- resultados()
+      plot <- ggplot(datos, aes(x=time,y=values, group=geo, colour=geo)) +
+        geom_point() + geom_line() +
+        coord_cartesian(xlim=c(2005:2017)) +
+        labs(x="Anio", y=toString(input$variable)) +
+        scale_x_continuous(breaks = c(2005,2007,2009,2011,2013,2015,2017))
+      plot
+      
+      
+      # datos <- resultados()
+      # plot <- ggplot(datos, aes(x=time,y=values, group=geo, colour=geo))
+      # plot <- plot + geom_point() + geom_line()
+      # plot <- plot + coord_cartesian(xlim=c(2005:2015))
+      # plot <- plot + labs(x="Anio", y="Concepto")
+      # plot
+    })
+  
+    
+  
+      
     output$tabla <- renderTable({
       datos <- resultados()
       
@@ -228,75 +268,177 @@ shinyServer(
     spacing = 'm',
     width = '20cm')
     
-    
-    output$downloadReport <- downloadHandler(
-      filename = function() {
-        paste('reporte', sep = '.', switch(
-          input$format, PDF = 'pdf'
-        ))
-      },
+    output$tabla2 <- renderTable({
+      datos <- resultados()
       
       
-      content = function(file) {
-        src <- normalizePath('Prueba_reporte.Rmd')
-        
-        # temporarily switch to the temp dir, in case you do not have write
-        # permission to the current working directory
-        owd <- setwd(tempdir())
-        on.exit(setwd(owd))
-        file.copy(src, 'Prueba_reporte.Rmd', overwrite = TRUE)
-        
-        #datos <- resultados()
-        datos <- resultados()
-        
-        datos %>% dplyr::select(geo, time, values) %>%
-          dplyr::group_by(geo) %>%
-          dplyr::mutate(var_porct = (values/lag(values) -1)*100) %>%
-          dplyr::filter(time >=2005) -> datos2P
-        
-        paises <- unique(datos2P$geo)
-        
-        
-        provisional <- list() 
-        
-        for (i in seq_along(paises)) {
-          provisional[[i]] <- datos2P %>%
-            dplyr::filter(geo==paises[i])
-        }
-        
-        Tabla <- provisional %>%
-          purrr::reduce(dplyr::full_join, by = "time") %>%
-          dplyr::select(time, geo.x, values.x:var_porct) -> Tabla  
-        
-        for(i in seq(3, 9, by = 3)){
-          colnames(Tabla)[i] <- as.character(paises[(i/3)])
-        }
-        
-        
-        Tabla %>%
-          dplyr::ungroup() %>%
-          dplyr::select(-dplyr::starts_with('geo')) -> Tabla
-        
-        
-        for(i in seq(3, 7, by = 2)){
-          colnames(Tabla)[i] <- "Variacion Porcentual"
-        }
-        
-        colnames(Tabla)[1] <- "Anio"
-        
-        
-        
-        Tablon <- Tabla 
-        
-        library(rmarkdown)
-        out <- render('Prueba_reporte.Rmd', switch(
-          input$format,
-          PDF = pdf_document()
-        ))
-        file.rename(out, file)
-        
+      # Seleccionamos las columnas que nos interesan (pa?s, anio y valores de la variable), agrupamos por pa?s y calculamos las variaciones porcentuales. Finalmente nos quedamos solo con los anios 2005 en adelante 
+      datos %>% dplyr::select(geo, time, values) %>%
+        dplyr::group_by(geo) %>%
+        dplyr::mutate(var_porct = (values/lag(values) -1)*100) %>%
+        dplyr::filter(time >=2005) -> datos2P
+      
+      #creamos un vector con los pa?ses
+      paises <- unique(datos2P$geo)
+      
+      #Tenemos una tabla 'flat' pero nos interesa obtener una tabla en la cual cada pa?s est? 'al lado del otro' para un anio dado. Para eso creamos una lista donde cada elemento son los datos correspondientes a cada pa?s (una especie de sub-tabla). Luego hacemos un join de las tres sub-tablas y ya tenemos el aspecto que deseamos
+      
+      provisional <- list() #Lista vac?a
+      
+      #creamos las sub-tablas para cada pa?s
+      for (i in seq_along(paises)) {  
+        provisional[[i]] <- datos2P %>%
+          dplyr::filter(geo==paises[i])
       }
-    )
+      
+      #Hacemos el join de las 3 sub-tablas y cambiamos el orden de las columnas para colocar el anio en la primera columna
+      Tabla <- provisional %>%
+        purrr::reduce(dplyr::full_join, by = "time") %>%
+        dplyr::select(time, geo.x, values.x:var_porct) -> Tabla  
+      
+      
+      # dat3 <- datos[,c("geo","time", "values")]
+      # #Los los paquetes para la var% son plyr y quantmod
+      # 
+      # #Calculamos la Var %
+      # variaciones <- ddply(dat3, "geo", transform,  DeltaCol = Delt(values))
+      # #cambio <- colnames(variaciones)[length(colnames(variaciones))]
+      # #Cambiamos el nombre a las columnas
+      # library("plyr")
+      # variaciones <- plyr:: rename(variaciones,c("geo"="Pais","time"="Year","values"="valor",
+      #                                            "Delt.1.arithmetic"="Var. Porcentual"))
+      # #Multiplicamos la variaci?n por 100 (%)
+      # variaciones$`Var. Porcentual`= variaciones$`Var. Porcentual`*100
+      # 
+      #Hacemos una tabla por pa?s
+      # paises <- unique(variaciones$Pais)
+      # pais1 <- variaciones[variaciones$Pais==paises[1],]
+      # pais2 <- variaciones[variaciones$Pais==paises[2],]
+      # pais3 <- variaciones[variaciones$Pais==paises[3],]
+      # paises2 <- as.character(paises)
+      # 
+      # #Montamos la tabla final
+      # TablaProv <- merge(pais1, pais2, by="Year", all=TRUE)
+      # Tabla <- merge(TablaProv, pais3, by="Year", all=TRUE)
+      
+      #Cambiamos el nombre de las columnas con los datos por el del pa?s correspondiente
+      for(i in seq(3, 9, by = 3)){  
+        colnames(Tabla)[i] <- as.character(paises[(i/3)])
+      }
+      
+      
+      #Eliminamos las columnas que traen el nombre del pa?s para cada observaci?n dado que no nos hacen falta. Antes de eso hay que hacer un ungroup, en caso contrario mantendr?a una de las columnas que deseamos eliminar, dado que dplyr mantiene la columna de agrupaci?n
+      Tabla %>%
+        dplyr::ungroup() %>%
+        dplyr::select(-dplyr::starts_with('geo')) -> Tabla
+      
+      
+      #Colocamos el nombre a la columna de variacion porcentual y anio
+      for(i in seq(3, 7, by = 2)){
+        colnames(Tabla)[i] <- "Variacion Porcentual"
+      }
+      
+      colnames(Tabla)[1] <- "Anio"
+      
+      #Cambiamos el anio a caracter, para que desaparezcan los decimales
+      Tabla[, c(1)] <- sapply(Tabla[, c(1)], as.character)
+      
+      Tablon <- Tabla
+      
+      
+      # #Eliminamos columnas
+      # for(i in seq(2, 6, by = 2)){
+      #   Tabla[,i] <- NULL
+      # }
+      # #Cambiamos los nombres de las columnas
+      # for(i in seq(2, 6, by = 2)){
+      #   names(Tabla)[i] <- paises2[i/2]
+      # }
+      # #Cambiamos el Anio a caracter, para que desaparezcan los decimales
+      # Tabla[, c(1)] <- sapply(Tabla[, c(1)], as.character)
+      # colnames(Tabla)[1]='Anio'
+      # colnames(Tabla)[3]='Variacion Porcentual'
+      # colnames(Tabla)[5]='Variacion  Porcentual'
+      # colnames(Tabla)[ncol(Tabla)]='Variacion Porcentual '
+      # #Datos a partir del 2005
+      # Tablon <- subset(Tabla, Tabla$Anio >= 2005)
+    },bordered = TRUE,
+    striped = TRUE,
+    align = 'c',
+    spacing = 'm',
+    width = '20cm')
+    
+    
+    
+    # output$downloadReport <- downloadHandler(
+    #   filename = function() {
+    #     paste('reporte', sep = '.', switch(
+    #       input$format, PDF = 'pdf'
+    #     ))
+    #   },
+    #   
+    #   
+    #   content = function(file) {
+    #     src <- normalizePath('Prueba_reporte.Rmd')
+    #     
+    #     # temporarily switch to the temp dir, in case you do not have write
+    #     # permission to the current working directory
+    #     owd <- setwd(tempdir())
+    #     on.exit(setwd(owd))
+    #     file.copy(src, 'Prueba_reporte.Rmd', overwrite = TRUE)
+    #     
+    #     #datos <- resultados()
+    #     datos <- resultados()
+    #     
+    #     datos %>% dplyr::select(geo, time, values) %>%
+    #       dplyr::group_by(geo) %>%
+    #       dplyr::mutate(var_porct = (values/lag(values) -1)*100) %>%
+    #       dplyr::filter(time >=2005) -> datos2P
+    #     
+    #     paises <- unique(datos2P$geo)
+    #     
+    #     
+    #     provisional <- list() 
+    #     
+    #     for (i in seq_along(paises)) {
+    #       provisional[[i]] <- datos2P %>%
+    #         dplyr::filter(geo==paises[i])
+    #     }
+    #     
+    #     Tabla <- provisional %>%
+    #       purrr::reduce(dplyr::full_join, by = "time") %>%
+    #       dplyr::select(time, geo.x, values.x:var_porct) -> Tabla  
+    #     
+    #     for(i in seq(3, 9, by = 3)){
+    #       colnames(Tabla)[i] <- as.character(paises[(i/3)])
+    #     }
+    #     
+    #     
+    #     Tabla %>%
+    #       dplyr::ungroup() %>%
+    #       dplyr::select(-dplyr::starts_with('geo')) -> Tabla
+    #     
+    #     
+    #     for(i in seq(3, 7, by = 2)){
+    #       colnames(Tabla)[i] <- "Variacion Porcentual"
+    #     }
+    #     
+    #     colnames(Tabla)[1] <- "Anio"
+    #     
+    #     
+    #     
+    #     Tablon <- Tabla 
+    #     
+    #     library(rmarkdown)
+    #     out <- render('Prueba_reporte.Rmd', switch(
+    #       input$format,
+    #       PDF = pdf_document()
+    #     ))
+    #     file.rename(out, file)
+    #     
+    #   }
+    # )
     
   }
 )
+
--- ShinyLabor/ui.R
+++ ShinyLabor/ui.R
@@ -1,12 +1,16 @@
 
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
+  tabsetPanel(
+    tabPanel("Paro", fluid=TRUE,
   headerPanel("Datos de Empleo (EUROSTAT)"),
   sidebarPanel(
     selectizeInput(
       "concepto", "Concepto",
-                choices = c('Paro'="une_rt_a",'Empleo y actividad'="lfsi_emp_a",'Paro de larga duración'="une_ltu_a",
-                            'Nivel de educación'="lfsi_educ_a",'Empleo temporal'="lfsi_pt_a")
+                choices = c('Paro'="une_rt_a"
+                            # ,'Empleo y actividad'="lfsi_emp_a",'Paro de larga duración'="une_ltu_a",
+                            # 'Nivel de educación'="lfsi_educ_a",'Empleo temporal'="lfsi_pt_a"
+                            )
                 ),
     selectizeInput(
       "pais","País",
@@ -26,23 +30,23 @@ shinyUI(pageWithSidebar(
                    ),
     selectizeInput(
       "indicador", "Indicador",
-      choices=c('Largo plazo'="LTU",'Muy largo plazo'="VLTU","UEMP","NSEE_AV",'Empleo'="EMP_LFS",'Población activa'="ACT"),
-      multiple = TRUE, options = list(maxItems = 2)
+      choices=c('Largo plazo'="LTU",'Muy largo plazo'="VLTU","UEMP","NSEE_AV",'Empleo'="EMP_LFS",'Población activa'="ACT")
+      #,multiple = TRUE, options = list(maxItems = 2)
     ),
     selectizeInput(
       "estacion", "Ajuste Estacional",
-      choices=c("TOTAL","Y15-64"),
-      multiple = TRUE, options = list(maxItems = 2)
+      choices=c("TOTAL","Y15-64")
+      #,multiple = TRUE, options = list(maxItems = 2)
     ),
     selectizeInput(
       "educacion", "Nivel de Educación",
-      choices=c('Primaria'="ED0-2",'Secundaria'="ED3_4",'Superior'="ED5-8"),
-      multiple = TRUE, options = list(maxItems = 2)
+      choices=c('Primaria'="ED0-2",'Secundaria'="ED3_4",'Superior'="ED5-8")
+      #,multiple = TRUE, options = list(maxItems = 2)
     ),
     selectizeInput(
       "worktime", "Tiempo de trabajo",
-      choices=c('Temporal'="TEMP",'Media jornada'="PT"),
-      multiple = TRUE, options = list(maxItems = 2)
+      choices=c('Temporal'="TEMP",'Media jornada'="PT")
+      #,multiple = TRUE, options = list(maxItems = 2)
     ),
     
     radioButtons('format', 'Formato del reporte', c('PDF'),
@@ -58,5 +62,56 @@ shinyUI(pageWithSidebar(
     plotOutput("grafico"),
     tableOutput("tabla")
   )
+  )
+  # ,#fin de tabpanel1
+  # 
+  # tabPanel("Empleo",fluid=TRUE,
+  #          headerPanel("Datos de Empleo (empleo)"),
+  #          sidebarPanel(
+  #            selectizeInput(
+  #              "concepto", "Concepto",
+  #              choices = c('Empleo y actividad'="lfsi_emp_a"
+  #                # ,'Paro'="une_rt_a",'Paro de larga duración'="une_ltu_a",
+  #                #           'Nivel de educación'="lfsi_educ_a",'Empleo temporal'="lfsi_pt_a"
+  #                )
+  #            ),
+  #            selectizeInput(
+  #              "pais","País",
+  #              choices=c('Grecia'="EL",'Alemania'="DE",'Bélgica'="BE",'Dinamarca'="DK",'Irlanda'="IE",'Italia'="IT",'Francia'="FR",'Países Bajos'="NL",'Portugal'="PT",'Austria'="AT",'Finlandia'="FI",'Suecia'="SE",'Reino Unido'="UK")
+  #            ),
+  #            selectizeInput(
+  #              "sexo", "Sexo",
+  #              choices=c('Total'="T", 'Mujeres'="F",'Hombres'="M")
+  #            ),
+  #            selectizeInput(
+  #              "unidad", "Unidad",
+  #              choices=c('% Población'="PC_POP")
+  #            ),
+  #            selectizeInput(
+  #              "edad", "Edad",
+  #              choices=c('Entre 15 y 64'="Y15-64")
+  #            ),
+  #            selectizeInput(
+  #              "indicador", "Indicador",
+  #              choices=c('Población activa'="ACT", 'Empleo'="EMP_LFS")
+  #              #,multiple = TRUE, options = list(maxItems = 2)
+  #            ),
+  #            
+  #            radioButtons('format', 'Formato del reporte', c('PDF'),
+  #                         inline = TRUE),
+  #            downloadButton('downloadReport')
+  # 
+  # 
+  #          ),
+  # 
+  # 
+  # 
+  #          mainPanel(
+  #            plotOutput("grafico2"),
+  #            tableOutput("tabla2")
+  #          )
+  # )#fin de tabpanel2
+
+  )#fin de tabsetpanel
 ))
 
